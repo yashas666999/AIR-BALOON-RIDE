1297f4631fcb910c332910f046d8255f761a0042
yashas666999 <108930241+yashas666999@users.noreply.github.com>
2022-11-11 18:07:19 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,77 @@
+var balloon,balloonImage1,balloonImage2;
+var database;
+var height;
+
+function preload(){
+   bg =loadImage("Images/cityImage.png");
+   balloonImage1=loadAnimation("Images/HotAirBallon-01.png");
+   balloonImage2=loadAnimation("Images/HotAirBallon-01.png","Images/HotAirBallon-01.png",
+   "Images/HotAirBallon-01.png","Images/HotAirBallon-02.png","Images/HotAirBallon-02.png",
+   "Images/HotAirBallon-02.png","Images/HotAirBallon-03.png","Images/HotAirBallon-03.png","Images/HotAirBallon-03.png");
+  }
+
+//Function to set initial environment
+function setup() {
+  database=firebase.database();
+  createCanvas(1500,700);
+
+  balloon=createSprite(250,650,250,650);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
+
+  var balloonHeight=database.ref('balloon/height');
+  balloonHeight.on("value",readHeight, console.log("error"));
+  textSize(20); 
+}
+
+// function to display UI
+function draw() {
+  background(bg);
+
+  if(keyDown(LEFT_ARROW)){
+    updateHeight(-10,0);
+    addAnimation("assets/ballonImage2/png");
+  }
+  else if(keyDown(RIGHT_ARROW)){
+    updateHeight(10,0);
+    addAnimation("assets/ballonImage2/png");
+  }
+  else if(keyDown(UP_ARROW)){
+    updateHeight(0,-10);
+    addAnimation("assets/ballonImage2/png");
+    balloon.scale=balloon.scale -0.005;
+  }
+  else if(keyDown(DOWN_ARROW)){
+    updateHeight(0,+10);
+    addAnimation("assets/ballonImage2/png");
+    balloon.scale=balloon.scale+0.005;
+  }
+
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**Use arrow keys to move Hot Air Balloon!",40,40);
+
+}
+
+
+function updateHeight(x,y){
+  database.ref('/balloon/height').update({
+    'x': height.x + x ,
+    'y': height.y + y
+  })
+}
+
+
+
+
+function readHeight(data){
+  data.value(height);
+  data.value(baloon, positionX);
+  data.value(baloon, positionY);
+ }
+
+function showError(){
+  console.log("Error in writing to the database");
+}
